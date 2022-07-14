3bef53de0f8f96be542c15ba49fcae8b31f700f5
peng1027 <peng1027@icloud.com>
2022-07-14 13:21:19 +0800

    Add files via upload

--- n2dreward.sol
+++ n2dreward.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract N2DRewards is ERC20, ERC20Burnable, Ownable {
+contract WJJToken is ERC20, ERC20Burnable, Ownable {
   using SafeMath for uint256;
 
   mapping(address => uint256) private _balances;
@@ -15,10 +15,10 @@ contract N2DRewards is ERC20, ERC20Burnable, Ownable {
 
   uint256 private _totalSupply;
   uint256 private MAXSUP;
-  uint256 constant MAXIMUMSUPPLY=1000000*10**18;
+  uint256 constant MAXIMUMSUPPLY=100000000*10**18;
 
-  constructor() ERC20("N2DRewards", "N2DR") { 
-      _mint(msg.sender, 1000000 * 10 ** 18);
+  constructor() ERC20("WJJ", "WJJ") { 
+      _mint(msg.sender, 100000000 * 10 ** 18);
 
   }
 
